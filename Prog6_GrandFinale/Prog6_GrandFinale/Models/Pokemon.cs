7526a6c544d87dc78057b68d5b401f7473968529
Stino <Linksonder@gmail.com>
2016-12-12 13:45:01 +0100

    Date attribute

--- Prog6_GrandFinale/Prog6_GrandFinale/Models/Pokemon.cs
+++ Prog6_GrandFinale/Prog6_GrandFinale/Models/Pokemon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,7 @@ namespace Prog6_GrandFinale.Models
 
         public string Type { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
         public int Level { get; set; }
