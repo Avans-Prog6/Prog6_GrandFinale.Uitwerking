927b40a21bfe8763703b020d24a20e48f1d102da
Stino <Linksonder@gmail.com>
2016-12-12 13:41:52 +0100

    pokemon controller

---
+++ Prog6_GrandFinale/Prog6_GrandFinale/Controllers/PokemonController.cs
@@ -0,0 +1,33 @@
+﻿using Prog6_GrandFinale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prog6_GrandFinale.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            var pokemons = new List<Pokemon>();
+            pokemons.Add(new Pokemon() { Name = "Pikachu", Level = 30 });
+            return View(pokemons);
+        }
+
+        public ActionResult Create()
+        {
+            var pokemon = new Pokemon();
+            return View(pokemon);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Pokemon pokemon)
+        {
+            //nu nog niks.
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Prog6_GrandFinale/Prog6_GrandFinale/Models/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prog6_GrandFinale.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Level { get; set; }
+    }
+}
